fe68956a47f90d91d25e5e4019749b6afb3bb9e0
Pesheva Irina <pesheva.i@yandex.ru>
2024-12-27 18:27:17 +0300

    Add basic atlantis structure

---
+++ variables.tf
@@ -0,0 +1,150 @@
+variable "sa_key_file_path" {
+  type        = string
+  default     = "~/key.json"
+  description = "The path to the service account key file"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+# VPC
+
+variable "vpc_name" {
+  type        = string
+  default     = "atlantis-vpc"
+  description = "Name of the VPC."
+}
+
+variable "vpc_subnets" {
+  type = list(object({
+    vpc_zone = string
+    vpc_cidr = string
+  }))
+  default = [
+    { vpc_zone = "ru-central1-d", vpc_cidr = "192.168.14.0/24" },
+  ]
+  description = "List of public subnets for the VPC, including zone and CIDR."
+}
+
+
+# VM
+
+variable "instances_count" {
+  type        = number
+  default     = 1
+  description = "Number of instances to create."
+}
+
+variable "instance_name" {
+  type        = string
+  default     = "atlantis"
+  description = "Name of the instance."
+}
+
+variable "image_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "The image family to use for the instance."
+}
+
+variable "has_nat" {
+  type        = bool
+  default     = true
+  description = "Whether to enable NAT for the instance."
+}
+
+variable "is_allowed_stopping_for_update" {
+  type        = bool
+  default     = true
+  description = "Whether to allow stopping the instance for updates."
+}
+
+variable "platform_id" {
+  type        = string
+  default     = "standard-v3"
+  description = "The platform ID for the instance."
+}
+
+variable "is_preemptible" {
+  type        = bool
+  default     = true
+  description = "Whether the instance is preemptible."
+}
+
+variable "resources" {
+  type = object({
+    cores         = number
+    memory        = number
+    core_fraction = number
+  })
+  default = {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+  description = "Resource configuration for the instance."
+}
+
+# Cloud init
+
+variable "cloud_init_path" {
+  type        = string
+  default     = "./cloud-init.yml"
+  description = "Path to the init file"
+}
+
+variable "vms_ssh_key" {
+  type        = string
+  description = "SSH key for cluster vms user"
+}
+
+variable "atlantis_version" {
+  type        = string
+  default     = "v0.32.0"
+  description = "Version of Atlantis Docker image"
+}
+
+variable "github_user" {
+  type        = string
+  default     = "RedRatInTheHat"
+  description = "Username of the Terraform repository owner"
+}
+
+variable "github_token" {
+  type        = string
+  description = "Token of the Terraform repository owner"
+}
+
+variable "webhook_secret" {
+  type        = string
+  description = "Secret of the github webhooks"
+}
+
+variable "github_repository" {
+  type        = string
+  default     = "github.com/RedRatInTheHat/terraform-for-k8s"
+  description = "Repository the Terraform project stored in"
+}
+
+variable "bucket_access_key" {
+  type        = string
+  description = "Access key for bucket access"
+}
+
+variable "bucket_secret_key" {
+  type        = string
+  description = "Secret key for bucket access"
+}
---
+++ vm.tf
@@ -0,0 +1,53 @@
+module "atlantis_vpc" {
+  source = "git::https://github.com/RedRatInTheHat/simple-vpc.git?ref=36f6f3c"
+
+  vpc_name    = var.vpc_name
+  vpc_subnets = var.vpc_subnets
+}
+
+module "atlantis_vm" {
+  source = "git::https://github.com/RedRatInTheHat/simple-vms.git?ref=1a892e1"
+
+  instances_count = var.instances_count
+
+  instance_name = var.instance_name
+  image_family  = var.image_family
+
+  subnets = [
+    for subnet in module.atlantis_vpc.subnet_info : {
+      "subnet_id" : subnet.id,
+      "subnet_zone" : subnet.zone
+    }
+  ]
+
+  has_nat                   = var.has_nat
+  allow_stopping_for_update = var.is_allowed_stopping_for_update
+  platform_id               = var.platform_id
+  is_preemptible            = var.is_preemptible
+
+  resources = {
+    cores         = var.resources.cores
+    memory        = var.resources.memory
+    core_fraction = var.resources.core_fraction
+  }
+
+  metadata = {
+    user-data = data.template_file.cloudinit.rendered
+  }
+}
+
+data "template_file" "cloudinit" {
+  template = file(var.cloud_init_path)
+
+  vars = {
+    ssh_key          = var.vms_ssh_key
+    atlantis_version = var.atlantis_version
+    gh_user          = var.github_user
+    gh_token         = var.github_token
+    webhook_secret   = var.webhook_secret
+    repo             = var.github_repository
+
+    bucket_access_key = var.bucket_access_key
+    bucket_secret_key = var.bucket_secret_key
+  }
+}
